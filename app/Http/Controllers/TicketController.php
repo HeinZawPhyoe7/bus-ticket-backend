caff991bb521d6ee2a09e943a1b5508af875c622
Hein12345 <heinzawphyoe2@gmail.com>
2025-03-24 23:49:03 +0700

    store and create-ticket

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ticket;
+use Illuminate\Http\Request;
+
+class TicketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $ticket = new Ticket();
+        $ticket->class = $request->class;
+        $ticket->time = $request->time;
+        $ticket->price = $request->price;
+        $ticket->seat = $request->seat;
+        $ticket->save();
+
+        return response()->json([
+            'message' => 'success',
+            'class' => $ticket->class,
+            'time' => $ticket->time,
+            'price' => $ticket->price,
+            'seat' => $ticket->seat
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
